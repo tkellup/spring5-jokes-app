768bbe6ac65e3ae68ca4a4e6f908242821a3fdc4
tkellup <tkellup@yahoo.com>
2020-10-01 10:42:48 -0500

    new joke app rep

---
+++ src/test/java/guru/framework/joke/jokeapp/JokeappApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.framework.joke.jokeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
